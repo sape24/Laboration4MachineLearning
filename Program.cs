1496f1df3c776db0afce14bac19a97fec437ebfd
Samuel Petersson <samuel.petersson@hotmail.com>
2026-01-11 15:08:49 +0100

    Kommenterad kod

--- Program.cs
+++ Program.cs
@@ -5,37 +5,37 @@ class Program
 {
     static void Main()
     {
-        while (true)   
+        while (true)   //loop som körs kontinuerligt så att användaren kan testa flera meningar
         {
             Console.Clear();
             PrintHeader();
             string input = Console.ReadLine();
 
-            if (string.IsNullOrWhiteSpace(input))
+            if (string.IsNullOrWhiteSpace(input))                   //avsluta programmet om användaren lämnar tomt och trycker enter
                 break;
 
-            var sampleData = new SentimentModel.ModelInput()
+            var sampleData = new SentimentModel.ModelInput()                //skapa ett input objekt som matchar formatet modellen tränades på
             {
                 Col0 = input
             };
 
-            var result = SentimentModel.Predict(sampleData);
+            var result = SentimentModel.Predict(sampleData);                 //skickar datan till ML modellen och får tillbaka ett resultat
 
-            float negativeScore = result.Score[1];
+            float negativeScore = result.Score[1];                             //hämtar scoren för de olika utfallen
             float positiveScore = result.Score[0];
 
-            string sentiment = result.PredictedLabel == 1
+            string sentiment = result.PredictedLabel == 1                  
                 ? " Positivt omdöme"
                 : " Negativt omdöme";
-            Console.WriteLine();
+            Console.WriteLine();                                              //presentation av resultatet 
             Console.WriteLine();
             Console.WriteLine(">>> Beräknar känslan av omdömet");
             Console.WriteLine();
             Console.WriteLine("Resultat");
             Console.WriteLine("---------------");
-            Console.WriteLine($"Text: {sampleData.Col0}");
+            Console.WriteLine($"Text: {sampleData.Col0}");                           //skriver ut den exakta sannolikheten
             Console.WriteLine($"Bedömning: {sentiment}");
-            Console.WriteLine($"Känsla                              Score");
+            Console.WriteLine($"Känsla                              Score");                               
             Console.WriteLine($"------                              --------");
             Console.WriteLine($"Positiv                             {positiveScore:0.000}");
             Console.WriteLine($"Negativ                             {negativeScore:0.000}");
@@ -48,7 +48,7 @@ class Program
         }
     }
 
-    static void PrintHeader()
+    static void PrintHeader()                                                   //hjälpmetod för att skriva ut en snygg header
     {
         Console.WriteLine("====================================================");
         Console.WriteLine("                 Sentimentanalys ML                 ");
